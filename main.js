6c0c3251ce93c1f130bccc0db3d9dab76afd538f
Chicodev30 <chicodev30@gmail.com>
2023-10-24 16:44:28 -0300

    atualizando versão

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import MousePosition from 'ol/control/MousePosition.js'; //adicionado na v01
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import { fromLonLat } from 'ol/proj';
 
-const map = new Map({           // declara um novo mapa
-  target: 'map',                // define o elemento HTML que contém o mapa
-  layers: [                     // define as camadas do mapa
-    new TileLayer({             // camada de azulejos
-      source: new OSM()         // fonte de azulejos OpenStreetMap
-    })
+const portoAlegreCoordinates = [-51.2177, -30.0392];
+
+const portoAlegreLonLat = fromLonLat(portoAlegreCoordinates);
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
   ],
-  view: new View({             // define a visualização do mapa
-    center: [0, 0],            // define o centro do mapa
-    zoom: 2                    // define o nível de zoom
-  })
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
 });
 
-// Adiciona o controle de posição do mouse - adicionado na v01
-const mousePositionControl = new MousePosition({  //cria uma nova instância do controle
-  coordinateFormat: function(coordinate) {        //define a função de formatação de coordenadas
-    return `Coordenadas: ${coordinate}`;          //retorna as coordenadas x e y do mouse do mapa
-  },
-  projection: 'EPSG:4326',                        //define a projeção do mapa - padrão é EPSG:3857
-  className: 'custom-mouse-position',             //define a classe CSS do elemento DOM que contém as coordenadas
-  target: document.getElementById('mouse-position'),//define o elemento DOM que contém as coordenadas                           //define o conteúdo HTML para as coordenadas quando o ponteiro do mouse está fora do mapa
+document.getElementById('zoom-out').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById('zoom-in').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
+
+// Adicione um evento de clique ao botão "Ir para Porto Alegre"
+const toPOAButton = document.getElementById('toPOA');
+toPOAButton.addEventListener('click', function() {
+  // Mude a visualização para Porto Alegre com zoom 13
+  map.getView().setCenter(portoAlegreLonLat);
+  map.getView().setZoom(13);
 });
 
-// Adiciona o controle ao mapa - adicionado na v01
-map.addControl(mousePositionControl); //adiciona o controle ao mapa
+const resetViewButton = document.getElementById('resetView');
+resetViewButton.addEventListener('click', function() {
+  map.getView().setCenter([0, 0]);
+  map.getView().setZoom(2);
+});
